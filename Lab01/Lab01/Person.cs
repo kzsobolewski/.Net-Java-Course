e7976d56ec560e131f376b62da7b8a233616309b
kzsobolewski <kzsobolewski@gmail.com>
2019-03-20 01:55:11 +0100

    Dodano 2 program

---
+++ Lab01/Lab01/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace Lab01
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string AvatarUri { get; set; }
+    }
+}
